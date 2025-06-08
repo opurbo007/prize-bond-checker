e6a68b636af90a897201ae53f145af77fe7db85c
opurbo007 <opurboujjol@gmail.com>
2025-06-08 10:55:33 +0600

    final fix

--- src/app/(main)/cards/[cardId]/page.tsx
+++ src/app/(main)/cards/[cardId]/page.tsx
@@ -1,5 +1,13 @@
-import CardDetails from "./CardDetails";
+import dynamic from "next/dynamic";
 
-export default function CardPage({ params }: { params: { cardId: string } }) {
+const CardDetails = dynamic(() => import("./CardDetails"), { ssr: false });
+
+interface PageProps {
+  params: {
+    cardId: string;
+  };
+}
+
+export default function CardPage({ params }: PageProps) {
   return <CardDetails cardId={params.cardId} />;
 }
