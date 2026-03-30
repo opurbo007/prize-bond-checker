7947dfbefafca889d857d87c5de7042f3a258077
Shuvoo943 <opurboujjol@gmail.com>
2026-03-30 16:05:58 +0600

    try now

--- src/components/BondCard.tsx
+++ src/components/BondCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
